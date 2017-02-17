d5d9a544a64ee4623a710f26eeb84fcecc49c211
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-17 15:19:59 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :hp
   attr_reader :id, :name, :type, :db
 
   def initialize(id:, db:, name:, type:)
@@ -15,5 +18,13 @@ class Pokemon
   def self.find(id, db)
     query = db.execute("SELECT * FROM pokemon WHERE id=#{id}")[0]
     new_pokemon = self.new(id:query[0], db:db, name: query[1], type:query[2])
+    if query.length > 3
+      new_pokemon.hp = query[-1]
+    end
+    return new_pokemon
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = #{hp}")
   end
 end
